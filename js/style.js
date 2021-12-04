05dad20b25e93d85161f81cdb9af34aa4dea0043
ksh <rlatngus03959@naver.com>
2021-12-04 11:16:47 +0900

    cursor modify

--- js/style.js
+++ js/style.js
@@ -28,9 +28,13 @@ for(let i=0; i<gnb.length; i++)
     gnb[i].addEventListener("click",(e)=>{
 
         e.preventDefault();
+        for(let i=0; i<gnb.length; i++)
+        {
+            gnb[i].classList.remove("on");
+        }
+        e.currentTarget.classList.add("on");
 
         let visualOffsettop = visuals[i].offsetTop;
-        console.log(visualOffsettop);
         window.scrollTo
         ({
             top:visualOffsettop,
@@ -40,7 +44,50 @@ for(let i=0; i<gnb.length; i++)
     });
 }
 
+//typing
+const typingtag = document.querySelector("#main .typing");
+let text = "SUHYUN'S\nPORTFOLIO";
+let viewText = "";
+let i=0;
+
+let autoTyping = setInterval(typing,300);
+
+function typing(){
+
+    viewText = text[i++];
+
+    if(viewText === "\n")
+    {
+        typingtag.innerHTML += "<br/>";
+    }
+    else
+    {
+        typingtag.innerHTML += viewText;
+    }
+
+    if(i > text.length-1)
+    {
+        clearInterval(autoTyping);
+
+        setTimeout(function(){
+
+            typingtag.innerHTML = "";
+            i=0;
+            autoTyping = setInterval(typing,300);
+        },2000);
+    }
+
+};
+
+//마우스커서
 
 
+window.addEventListener("mousemove",(e)=>{
 
+    let posX = e.pageX;
+    let posY = e.pageY;
+
+    document.querySelector(".mousecursor").style.left = posX+"px";
+    document.querySelector(".mousecursor").style.top = posY+"px";
+});
 
